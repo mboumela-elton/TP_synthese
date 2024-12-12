ff493567d8c96623224e99d64a036022bfed2df4
mboumela-elton <mboumelae@gmail.com>
2024-12-12 08:27:45 +0100

    test question 7

--- utils.c
+++ utils.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
 
 #include "constant.c"
 
@@ -59,37 +61,76 @@ void intToStr(int num, char *str)
     }
 }
 
-void prompt(int code, char *str, char *buffer, char *numStr)
+void prompt(int code, char *str, char *buffer, char *numStr, long timeTaken, char *timeStr)
 {
     strcpy(buffer, str);
     intToStr(code, numStr);
     strcat(buffer, numStr);
-    strcat(buffer, "] % ");
+    strcat(buffer, "|");
+    intToStr(timeTaken, timeStr);
+    strcat(buffer, timeStr);
+    strcat(buffer, "ms] % ");
 }
 
 void execSingleCommand(char *command)
 {
     int status;
+    struct timespec start, end;
     pid_t pid = fork();
     if (pid == 0)
     {
-        execlp(command, command, NULL);
+        clock_gettime(CLOCK_REALTIME, &start);
+
+        // Tokenize the command string into arguments
+        char *args[256];
+        int i = 0;
+        args[i] = strtok(command, " ");
+        while (args[i] != NULL)
+        {
+            i++;
+            args[i] = strtok(NULL, " ");
+        }
+        // Check for redirection operators
+        for (int j = 0; j < i; j++)
+        {
+            if (strcmp(args[j], ">") == 0)
+            {
+                int fd = open(args[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
+                args[j] = NULL;
+                break;
+            }
+            else if (strcmp(args[j], "<") == 0)
+            {
+                int fd = open(args[j + 1], O_RDONLY);
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+                args[j] = NULL;
+                break;
+            }
+        }
+
+        execvp(args[0], args);
         printMessage(COMMAND_ERROR);
         _exit(1);
     }
     else if (pid > 0)
     {
         wait(&status);
+        clock_gettime(CLOCK_REALTIME, &end);
+        long timeTaken = (end.tv_sec - start.tv_sec) * 1e3 + (end.tv_nsec - start.tv_nsec) / 1e6;
         char buffer[256];
         char numStr[12];
+        char timeStr[12];
 
         if (WIFEXITED(status))
         {
-            prompt(WEXITSTATUS(status), PROMPT_EXIT, buffer, numStr);
+            prompt(WEXITSTATUS(status), PROMPT_EXIT, buffer, numStr, timeTaken, timeStr);
         }
         else if (WIFSIGNALED(status))
         {
-            prompt(WTERMSIG(status), PROMPT_SIGN, buffer, numStr);
+            prompt(WTERMSIG(status), PROMPT_SIGN, buffer, numStr, timeTaken, timeStr);
         }
         printMessage(buffer);
     }
