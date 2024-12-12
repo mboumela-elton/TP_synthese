354cd0607ecbbd4f040af3f83c5672d284e86b3b
mboumela-elton <mboumelae@gmail.com>
2024-12-12 01:58:55 +0100

    exit or signal identifier added

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
-#include "enseash.c"
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-int main() {
-    enseash();
+#include "utils.c"
+#include "constant.c"
+
+int main()
+{
+    char command[256];
+
+    printMessage(WELCOME_MESSAGE);
+    printMessage(ENSEASH);
+
+    while (1)
+    {
+
+        // Read the command entered by the user
+        int bytes_read = readCommandLine(command);
+
+        // Remove the newline character
+        command[bytes_read - 1] = '\0';
+
+        if (bytes_read <= 1)
+        {
+            continue;
+        }
+
+        // Check for the 'exit' command
+        if (strcmp(command, EXIT) == 0)
+        {
+            break;
+        }
+        execSingleCommand(command);
+    }
+
+    printMessage(GOODBYE_MESSAGE);
     return 0;
 }
--- utils.c
+++ utils.c
@@ -14,8 +14,54 @@ ssize_t readCommandLine(char *command)
     return read(STDIN_FILENO, command, sizeof(command) - 1);
 }
 
+void intToStr(int num, char *str)
+{
+    int i = 0;
+    int isNegative = 0;
+
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+
+    if (num < 0)
+    {
+        isNegative = 1;
+        num = -num;
+    }
+
+    while (num != 0)
+    {
+        int rem = num % 10;
+        str[i++] = rem + '0';
+        num = num / 10;
+    }
+
+    if (isNegative)
+    {
+        str[i++] = '-';
+    }
+
+    str[i] = '\0';
+
+    // Reverse the string
+    int start = 0;
+    int end = i - 1;
+    while (start < end)
+    {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
 void execSingleCommand(char *command)
 {
+    int status;
     pid_t pid = fork();
     if (pid == 0)
     {
@@ -25,7 +71,27 @@ void execSingleCommand(char *command)
     }
     else if (pid > 0)
     {
-        wait(NULL);
+        wait(&status);
+        char buffer[256];
+        char numStr[12];
+
+        if (WIFEXITED(status))
+        {
+            int exit_code = WEXITSTATUS(status);
+            strcpy(buffer, "enseash [exit:");
+            intToStr(exit_code, numStr);
+            strcat(buffer, numStr);
+            strcat(buffer, "] % ");
+        }
+        else if (WIFSIGNALED(status))
+        {
+            int signal_num = WTERMSIG(status);
+            strcpy(buffer, "enseash [sign:");
+            intToStr(signal_num, numStr);
+            strcat(buffer, numStr);
+            strcat(buffer, "] % ");
+        }
+        printMessage(buffer);
     }
     else
     {
